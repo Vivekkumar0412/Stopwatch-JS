3a194ed69ec8a80957ad2c91108979ab01f12157
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-06-16 04:17:03 +0530

    whole funcanality added, Hour , minutes and second digits added and make both the buttons working

--- stopwatcah.js
+++ stopwatcah.js
@@ -1,1 +1,35 @@
 // alert("hi");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+
+let sc = document.getElementById("sc");
+let hour = document.getElementById("hour");
+let min = document.getElementById("min");
+let hur = document.getElementById("hur");
+
+let mainDiv = document.getElementById("main");
+let sec = 0;
+let mini = 0;
+let hr = 0;
+let str;
+
+btn1.addEventListener("click", function () {
+  str = setInterval(() => {
+    sc.innerHTML = sec;
+    sec++;
+    if (sec > 4) {
+      min.innerHTML = mini;
+      mini++;
+      sec = 0;
+      if (mini > 4) {
+        hur.innerHTML = hr;
+        hr++;
+        mini = 0;
+      }
+    }
+  }, 1000);
+});
+
+btn2.addEventListener('click',function(){
+    clearInterval(str);
+})
